a78e1ebbbc5c1cf83de4c0c7a55f83e2962fa145
janhaede <janhaede2@gmail.com>
2023-08-11 23:14:28 +0200

    Added a hero interface and added mock data to display in a list looping the heroes component

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Iron Man' },
+  { id: 13, name: 'Incredible Hulk' },
+  { id: 14, name: 'Captain America' },
+  { id: 15, name: 'Thor' },
+  { id: 16, name: 'Vision' },
+  { id: 17, name: 'Scarlet Witch' },
+  { id: 18, name: 'Black Widow' },
+  { id: 19, name: 'Bat Man' },
+  { id: 20, name: 'Spider Man' },
+];
